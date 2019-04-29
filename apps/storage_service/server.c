c431563cda7d4b61f55665e7627bcc5d1333558a
Noah Moroze <moroze@mit.edu>
2019-04-29 14:03:21 -0400

    Tweaks to server

--- apps/storage_service/server.c
+++ apps/storage_service/server.c
@@ -6,61 +6,75 @@
 #include "runtime/tcp.h"
 #include "runtime/thread.h"
 
-// #include "storage_service.h"
 #include "reflex.h"
 
 #define STORAGE_SERVICE_PORT 5000
-#define SECTOR_SIZE 512 
+#define SECTOR_SIZE 512
 
-static void server_worker(void *arg)
-{
+static void server_worker(void *arg) {
     tcpconn_t *c = (tcpconn_t *)arg;
-    binary_header_blk_t header;
     ssize_t ret;
 
+    binary_header_blk_t header;
     char *buf = storage_zmalloc(SECTOR_SIZE);
+    if (buf == NULL) {
+        log_err("storage_zmalloc: out of memory!");
+        return;
+    }
+
+    struct iovec response[2];
+    response[0].iov_base = &header;
+    response[0].iov_len = sizeof(header);
+    response[1].iov_base = buf;
+    response[1].iov_len = SECTOR_SIZE;
 
     while (true) {
         ret = tcp_read(c, &header, sizeof(header));
-        // log_info("received: %ld bytes", ret);
         if (ret <= 0 || ret < sizeof(header)) {
             break;
         }
 
-        // log_info("received command");
-        // log_info("magic: %u, lba: %lu, lba_count: %u", header.magic, header.lba, header.lba_count);
+        BUG_ON(header.magic != sizeof(binary_header_blk_t));
+        BUG_ON((header.opcode != CMD_GET) && (header.opcode != CMD_SET));
 
-        // BUG_ON(header.magic != sizeof(binary_header_blk_t));
-        // BUG_ON((header.opcode != CMD_GET) && (header.opcode != CMD_SET));
-        //
-        if(header.magic != sizeof(binary_header_blk_t)) {
-            // printf("skipped\n");
-            // printf("%s\n", (char*)&header);
-            continue;
-        }
+        // currently only handling workloads where lba_count = 1
         BUG_ON(header.lba_count != 1);
+
         uint32_t len = header.lba_count * SECTOR_SIZE;
         if (header.opcode == CMD_GET) {
-            log_info("get");
-            storage_read(buf, header.lba, header.lba_count);
-            tcp_write(c, &header, sizeof(header));
-            tcp_write(c, buf, len);
+            if (storage_read(buf, header.lba, header.lba_count)) {
+                log_err("storage_read failed");
+                break;
+            }
+            ret = tcp_writev(c, response, 2);
+            if (ret < 0) {
+                log_err("tcp_writev failed");
+                break;
+            }
         }
         if (header.opcode == CMD_SET) {
-            log_info("set");
             uint32_t read = 0;
-            // log_info("set len: %u", len);
             do {
                 ret = tcp_read(c, buf+read, len-read);
                 read += ret;
-                // log_info("ret: %lu", ret);
             } while(ret > 0 && len > read);
-            // log_info("set data: %s", buf);
-            storage_write(buf, header.lba, header.lba_count);
-            tcp_write(c, &header, sizeof(header));
+            if (ret < 0) {
+                log_err("tcp_read failed");
+            }
+
+            if (storage_write(buf, header.lba, header.lba_count)) {
+                log_err("storage_write failed");
+                break;
+            }
+            ret = tcp_write(c, &header, sizeof(header));
+            if (ret < 0) {
+                log_err("tcp_write failed");
+                break;
+            }
         }
     }
     storage_free(buf);
+    tcp_close(c);
 }
 
 static void main_handler(void *arg)
@@ -73,14 +87,12 @@ static void main_handler(void *arg)
     addr.port = STORAGE_SERVICE_PORT;
 
     ret = tcp_listen(addr, 4096, &q);
-    log_info("listening");
     BUG_ON(ret);
 
     while (true) {
         tcpconn_t *c;
 
         ret = tcp_accept(q, &c);
-        log_info("accepted connection");
         BUG_ON(ret);
         ret = thread_spawn(server_worker, c);
         BUG_ON(ret);
