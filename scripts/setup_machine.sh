ed33c7f3ae7009e31e3d1edea1ff428622785ddd
Inho Cho <inhocho@csail.mit.edu>
2019-07-22 12:55:04 -0600

    (1) disable c-state (2) disable frequency scaling (3) disable turbo-boost

--- scripts/setup_machine.sh
+++ scripts/setup_machine.sh
@@ -8,6 +8,7 @@ sysctl -w vm.hugetlb_shm_group=27
 sysctl -w vm.max_map_count=16777216
 sysctl -w net.core.somaxconn=3072
 
+# build ksched.ko
 cd ksched && make && cd ..
 
 # set up the ksched module
@@ -22,3 +23,16 @@ echo 8192 > /sys/devices/system/node/node0/hugepages/hugepages-2048kB/nr_hugepag
 for n in /sys/devices/system/node/node[1-9]; do
 	echo 0 > $n/hugepages/hugepages-2048kB/nr_hugepages
 done
+
+# turn on cstate
+sudo killall cstate
+cd scripts
+gcc cstate.c -o cstate
+./cstate 0 &
+cd ..
+
+# Disable frequency scaling
+echo performance | tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
+
+# Disable turbo boost
+echo 1 | tee /sys/devices/system/cpu/intel_pstate/no_turbo
