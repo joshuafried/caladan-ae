f2979c3f5423581ababdbd77d928d753a40238ed
Inho Cho <inhocho@csail.mit.edu>
2019-09-01 19:10:17 -0600

    draw figure script have p99 figures

--- scripts/draw_fig.py
+++ scripts/draw_fig.py
@@ -4,6 +4,7 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import csv
 
+
 tx = []
 ty = []
 
@@ -13,11 +14,14 @@ qy = []
 wx = []
 wy = []
 
+lx = []
+ly = []
+
 with open('agt.out') as agt_out:
     plots = csv.reader(agt_out, delimiter=',')
     for row in plots:
         tx.append(float(row[0]))
-        ty.append(float(row[1]))
+        ty.append(float(row[1])*1000.0)
 
 with open('q.out') as q_out:
     plots = csv.reader(q_out, delimiter=',')
@@ -31,22 +35,40 @@ with open('cwnd.out') as cwnd_out:
         wx.append(float(row[0]))
         wy.append(float(row[1]))
 
-plt.subplot(3,1,1)
-plt.gca().set_ylim(bottom=0, top = 2)
+with open('p99.out') as lat_out:
+    plots = csv.reader(lat_out, delimiter=',')
+    for row in plots:
+        lx.append(float(row[0]))
+        ly.append(float(row[1]))
+
+plt.gcf().set_size_inches(10, 12)
+
+plt.subplot(4,1,1)
+plt.xticks(np.arange(0.0, 21.0, 2.0))
+plt.gca().set_ylim(bottom=0, top = 1500)
 plt.plot(tx, ty, label='throughput')
-plt.xlabel('Time (ms)')
-plt.ylabel('Throughput (MRPS)')
+plt.xlabel('Time (s)')
+plt.ylabel('Throughput (kRPS)')
 
-plt.subplot(3,1,2)
+plt.subplot(4,1,2)
+plt.xticks(np.arange(0.0, 21.0, 2.0))
+plt.gca().set_ylim(bottom=0, top = 2000)
+plt.plot(qx, qy, label='queueing delay')
+plt.xlabel('Time (s)')
+plt.ylabel('Queueing delay')
+
+plt.subplot(4,1,3)
+plt.xticks(np.arange(0.0, 21.0, 2.0))
 plt.gca().set_ylim(bottom=0, top = 10)
-plt.plot(qx, qy, label='standing queue')
-plt.xlabel('Time (ms)')
-plt.ylabel('Standing QLen')
-
-plt.subplot(3,1,3)
-plt.gca().set_ylim(bottom=0, top = 5)
 plt.plot(wx, wy, 'o-', markersize=3, label='cwnd')
-plt.xlabel('Time (ms)')
+plt.xlabel('Time (s)')
 plt.ylabel('cwnd')
+
+plt.subplot(4,1,4)
+plt.xticks(np.arange(0.0, 21.0, 2.0))
+plt.gca().set_ylim(bottom=0, top = 4000)
+plt.plot(lx,ly, label='latency')
+plt.xlabel('Time (s)')
+plt.ylabel('99p Latency')
 
 plt.savefig("out.png")
