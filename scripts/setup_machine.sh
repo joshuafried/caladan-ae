5a805b40602f079d75dd0a356fbe84dd2327b0fd
Inho Cho <inhocho@csail.mit.edu>
2020-02-12 13:14:07 -0700

    automated ksched make, cstate execution, disabling frequency scaling and turbo boost

--- scripts/setup_machine.sh
+++ scripts/setup_machine.sh
@@ -8,6 +8,9 @@ sysctl -w vm.hugetlb_shm_group=27
 sysctl -w vm.max_map_count=16777216
 sysctl -w net.core.somaxconn=3072
 
+# build ksched.ko
+cd ksched && make && cd ..
+
 # set up the ksched module
 rmmod ksched
 rm /dev/ksched
@@ -38,3 +41,16 @@ fi
 
 # enable RDPMC instruction from userspace
 echo 2 > /sys/devices/cpu/rdpmc
+
+# turn on cstate
+sudo killall cstate
+cd scripts
+gcc cstate.c -o cstate
+./cstate 0 &
+cd ..
+
+# Disable frequency scaling
+echo performance | tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
+
+# Disable turbo boost
+echo 1 | tee /sys/devices/system/cpu/intel_pstate/no_turbo
